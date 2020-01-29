33a1ca8d0ed9056ddc31ac92854789fd97756c3b
Jenaleigh <59895752+Jenaleigh@users.noreply.github.com>
2020-01-29 13:42:39 -0800

    updated urls.py and views.py to include Meeting index

--- pythonclubapp/urls.py
+++ pythonclubapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('getMeeting/', views.getMeeting, name='meeting'),
 ]
 
 from django.urls import path
--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Resource, ResourceType, Event
 
 # Create your views here.
 from django.shortcuts import render
@@ -7,3 +8,7 @@ from django.shortcuts import render
 def index (request):
     return render(request, 'pythonclubapp/index.html')
 
+def getMeeting(request):
+    type_list=Meeting.objects.all()
+    return render(request, 'pythonclubapp/meeting.html',{'type_list': type_list})
+    
