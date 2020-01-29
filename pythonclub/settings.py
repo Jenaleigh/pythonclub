f23731d18a0de44b4da47712c057338473ef6119
Jenaleigh <59895752+Jenaleigh@users.noreply.github.com>
2020-01-29 13:49:33 -0800

    Update settings.py

--- pythonclub/settings.py
+++ pythonclub/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pythonclubdb',
         'USER': 'postgres',
-        'PASSWORD': '509Cfimy2090',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
