3e3df0b4ce043bba2d495e44e817932040b1d59c
Jenaleigh <59895752+Jenaleigh@users.noreply.github.com>
2020-01-29 13:50:27 -0800

    Deleted redundancies in urls.py

--- pythonclubapp/urls.py
+++ pythonclubapp/urls.py
@@ -3,12 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('getMeeting/', views.getMeeting, name='meeting'),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('', views.index, name='index'),
+    path('getMeeting/', views.getMeeting, name='meetings'),
 ]
