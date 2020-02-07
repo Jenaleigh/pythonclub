e8713abfe0ad1aeb74873ad4eb7ba880e6dc4cfb
Jenaleigh <59895752+Jenaleigh@users.noreply.github.com>
2020-02-06 17:56:29 -0800

    Added login and logout

--- pythonclubapp/urls.py
+++ pythonclubapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('getMeeting/', views.getMeeting, name='meetings'),
     path('getResourceType/', views.getResourceType, name='resourcetypes'),
-    path('getResource/', views.getResource, name='resources')
+    path('getResource/', views.getResource, name='resources'),
+    path('loginMessage/', views.loginmessage, name='loginmessage'),
+    path('logoutMessage/', views.logoutmessage, name='logoutmessage'),
 ]
--- pythonclubapp/views.py
+++ pythonclubapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Meeting, MeetingMinutes, Resource, ResourceType, Event
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.shortcuts import render
@@ -8,6 +9,7 @@ from django.shortcuts import render
 def index (request):
     return render(request, 'pythonclubapp/index.html')
 
+@login_required
 def getMeeting(request):
     type_list=Meeting.objects.all()
     return render(request, 'pythonclubapp/meeting.html',{'type_list': type_list})
@@ -19,4 +21,10 @@ def getResourceType(request):
 def getResource(request):
     type_list=Resource.objects.all()
     return render(request, 'pythonclubapp/resource.html', {'type_list': type_list})
+
+def loginMessage(request):
+    return render(request, 'pythonclubapp/loginmessage.html')
+
+def logoutMessage(request):
+    return render(request, 'pythonclubapp/logoutmessage.html')
 
